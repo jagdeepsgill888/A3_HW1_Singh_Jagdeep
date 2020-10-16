9e30859a9dd0fd2d4eaac22530b4fe9684ba40c3
jagdeepsgill888 <59809683+jagdeepsgill888@users.noreply.github.com>
2020-10-16 15:34:21 -0400

    adding js function to handle course data with ajax

--- js/main.js
+++ js/main.js
@@ -8,7 +8,8 @@
         
     
         debugger;
-        console.log(data);
+        console.log(data)
+        handleDataSet(data);
     //handleData
     // here's where you would call the function that puts the pieces on the page
     })
@@ -17,6 +18,23 @@
     console.log(err);
     })
 
-    
+    function handleDataSet(data) {
+
+        let courseName = document.querySelector('.course-name');
+        courseName.innerHTML = data.coursename + " - ";
+          
+        let courseCode = document.querySelector('.course-code');
+        courseCode.innerHTML = data.coursecode;
+
+        let profName = document.querySelector('.prof-name');
+        profName.innerHTML = "Professor - " + data.profname;
+
+        let classTime = document.querySelector('.list-inline').children;
+        classTime[0].textContent = "🕐 " + data.classtime[0];
+        classTime[1].textContent = "🕘 " + data.classtime[1];
+
+        let courseDes = document.querySelector('.course-des');
+        courseDes.innerHTML = data.coursename + " - " + data.coursecode;
+    }
 
 })();
